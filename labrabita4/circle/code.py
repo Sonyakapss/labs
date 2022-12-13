9410a9a43bad739c4ab0eca85c00998676122156
Sonyakapss <113242718+Sonyakapss@users.noreply.github.com>
2022-12-14 00:18:49 +0300

    Update code.py

--- labrabita4/circle/code.py
+++ labrabita4/circle/code.py
@@ -1,1 +1,9 @@
+default_radius = 5
 
+#Периметр
+def circle_perimeter(radius=default_radius):
+    return (2*3,14*radius)
+    
+#Площадь
+def circle_area(radius=default_radius):
+    return (3,14*(radius**2))
