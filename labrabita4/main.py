95cbcf3a9bd79ef3fecb81c72231ff29aedae1b9
Sonyakapss <113242718+Sonyakapss@users.noreply.github.com>
2022-12-14 00:00:07 +0300

    Update main.py

---
+++ labrabita4/main.py
@@ -0,0 +1,9 @@
+from .circle import circle_perimeter, circle_area
+from .square import square_perimeter, square_area
+from .triangle import triangle_perimeter, triangle_area
+
+__all__ = [
+    'circle_area', 'circle_perimeter',
+    'square_area', 'square_perimeter',
+    'triangle_area','triangle_perimeter'
+         ]
