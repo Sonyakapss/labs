b25657177e2388a745f15edeeefdfab4d7f1d7de
Sonyakapss <113242718+Sonyakapss@users.noreply.github.com>
2022-12-14 00:17:13 +0300

    Update code.py

--- labrabita4/triangle/code.py
+++ labrabita4/triangle/code.py
@@ -1,1 +1,12 @@
+a = 7
+b = 2
+c = 8
 
+#Периметр
+def triangle_perimeter(default_a=a, default_b=b, default_c=c):
+    return (default_a + default_b + default_c)
+
+#Площадь
+def triangle_area(default_a=a, default_b=b, default_c=c):
+    p = (default_a + default_b + default_c)/2
+    return ((p * (p - default_a) * (p - default_b) * (p - default_c))**0,5)
